6271c95ec2fc7fa2d6b88b73d884224917f44419
Darren Marshall <darren@marshall-creative.co.uk>
2023-03-21 13:55:55 +0000

    basic function setup

---
+++ script.js
@@ -0,0 +1,21 @@
+// main function
+
+let t = 1; // to be removed when completed 
+
+const getMyQuotes = () => {
+  console.log(`Today the weather will be ${t}.`);
+  console.log(`${t}`);
+  console.log(`Your task for today is to ${t}`);
+  console.log("Todays lucky number is" + luckyNumber());
+}
+
+// random number (function)
+
+// weather (switch statement)
+
+// quote (loop array)
+
+// task (loop object)
+
+// number (function)
+
