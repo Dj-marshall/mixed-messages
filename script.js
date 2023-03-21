898d8e971ffbbd9337d9d0376b301aa6ca907651
Darren Marshall <darren@marshall-creative.co.uk>
2023-03-21 13:56:27 +0000

    added lucky number function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getMyQuotes = () => {
   console.log(`Today the weather will be ${t}.`);
   console.log(`${t}`);
   console.log(`Your task for today is to ${t}`);
-  console.log("Todays lucky number is" + luckyNumber());
+  console.log("Todays lucky number is " + luckyNumber());
 }
 
 // random number (function)
@@ -19,3 +19,11 @@ const getMyQuotes = () => {
 
 // number (function)
 
+function luckyNumber () {
+  return Math.floor(Math.random() * 10);
+}
+
+
+
+
+getMyQuotes();
